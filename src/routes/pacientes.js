4bb73ab728b9c4e05c12f8a81e77d25416e216fe
Kamamura <kefev23@gmail.com>
2023-04-28 18:06:52 -0500

    Consulta de cita agregada.
    Agregué lo que creo es una "consulta de cita".
    No voy a mentir, no estoy seguro si funciona correctamente-
    Si algo en las pruebas de postman probamos

--- src/routes/pacientes.js
+++ src/routes/pacientes.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
-const sesionSchema = require("../models/pacientes.js");
+const pacientesSchema = require("../models/pacientes.js");
 
-router.post("/empleados", (req, res) => {
-    const empleado = empleadosSchema(req.body);
-    empleado
+//Nuevo paciente
+
+router.post("/pacientes", (req, res) => {
+    const pacientes = pacientesSchema(req.body);
+    pacientes
         .save()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
 module.exports = router;
+
+//Consultar pacientes
+
+router.get("/pacientes", (req, res) => {
+    pacientesSchema.find().then((data) => res.json(data)).catch((error) => res.json({ mensaje: error }));
+})
+
+//consultar citas
+
+router.get("/pacientes/:", (req, res) => {
+
+    const { citas } = req.body
+
+    pacientesSchema.findOne({ citas })
+        .then(cita =>{
+            if(cita){
+                res.json({message: "Usted tiene una cita el día: "+cita})
+            }
+        })
+        .catch((error) => req.jason({message: error}));
+
+})
