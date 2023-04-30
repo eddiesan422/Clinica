fe20bb3fb545aca6ee1115fb8e76fb3d0058c827
Kamamura <kefev23@gmail.com>
2023-04-29 21:20:58 -0500

    Aún más correcciones de conflictos para merge

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const sesionRoutes = require("./routes/sesion");
+const citasRoutes = require("./routes/citas");
+const usuariosRoutes = require("./routes/usuarios")
+const medicinasRoutes = require("./routes/medicinas") // importa las rutas de sesiones
 const mongoose = require("mongoose");
 require('dotenv').config();
 app.use(parser.urlencoded({ extended: false })); //permite leer los datos que vienen en la petición
 app.use(parser.json()); // transforma los datos a formato JSON
 //Gestión de las rutas usando el middleware
+app.use("/api", sesionRoutes); // usa "sesionRoutes" en lugar de "librosRoutes"
+app.use("/api", citasRoutes);
+app.use("/api", usuariosRoutes);
+app.use("/api", medicinasRoutes);
 app.use(express.json());
 //Conexión a la base de datos
 mongoose
