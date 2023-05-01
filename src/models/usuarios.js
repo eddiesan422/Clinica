3b3b8fa42a29e5604a633062fa4b75ebb1875ea6
EstebanWoff <82189162+EstebanWoff@users.noreply.github.com>
2023-04-30 21:44:02 -0500

    Se cambiaron los parametros del modelo de usuarios

--- src/models/usuarios.js
+++ src/models/usuarios.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
 const usuarioSchema = new mongoose.Schema({
-  nombres: { type: String, required: true },
-  apellidos: { type: String, required: true },
-  edad: { type: Number, required: true },
   correo: { type: String, required: true },
   password: { type: String, required: true },
+  tipoUsuario: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Usuario', usuarioSchema);
